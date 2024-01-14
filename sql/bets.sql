b955633cf90be604e56bdcad690601c1294e8feb
Danmascus <Danielelmm@outlook.com>
2024-01-14 17:32:39 +1100

    mend

--- sql/bets.sql
+++ sql/bets.sql
@@ -1,10 +1,12 @@
-CREATE TABLE Bet
+CREATE TABLE Bets
 (
     id            SERIAL PRIMARY KEY,
-    match_id      INTEGER          NOT NULL,
-    user_id       INTEGER          NOT NULL,
-    team_bet_on   VARCHAR(255) CHECK (team_bet_on = 'teamOne' OR team_bet_on = 'teamTwo'),
-    odds_bet_with DOUBLE PRECISION NOT NULL,
-    currency_bet  DOUBLE PRECISION NOT NULL,
+    game_id       VARCHAR(255) NOT NULL,
+    user_id       INTEGER      NOT NULL,
+    team_id       VARCHAR(255) NOT NULL,
+    odds_bet_with VARCHAR(255) NOT NULL,
+    currency_bet  VARCHAR(255) NOT NULL,
+    state         VARCHAR(255) CHECK (state = 'PENDING' OR state = 'WON' OR state = 'LOST'),
+    created       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (userId)
 );
--- sql/users.sql
+++ sql/users.sql
@@ -1,11 +1,12 @@
-CREATE TABLE users (
-    userId SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    salt VARCHAR(255) NOT NULL,
-    currency DOUBLE PRECISION DEFAULT 0,
-    bets_won INTEGER DEFAULT 0,
-    bets_lost INTEGER DEFAULT 0,
-    created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE users
+(
+    userId        SERIAL PRIMARY KEY,
+    username      VARCHAR(255) UNIQUE NOT NULL,
+    password      VARCHAR(255)        NOT NULL,
+    salt          VARCHAR(255)        NOT NULL,
+    currency      VARCHAR(255)             DEFAULT '0',
+    bets_won      INTEGER                  DEFAULT 0,
+    bets_lost     INTEGER                  DEFAULT 0,
+    created       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     refresh_token VARCHAR(255)
 );
